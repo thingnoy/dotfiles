55c6e93f06f83563cf93d017eabb75a71cb3b6df
supasai haplakhon <supasai@hiqfood.com>
2022-02-07 00:33:26 +0700

    fix: homebrew can't install

--- setup/macos/01-install-homebrew.sh
+++ setup/macos/01-install-homebrew.sh
@@ -3,7 +3,7 @@
 if ! command -v brew &> /dev/null
 then
   echo "🟣 installing homebrew."
-  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+  yes | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   echo "✅ homebrew installed!"
 else
   echo "🟣 homebrew is installed."
