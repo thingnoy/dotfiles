ce65551d95a050acd893d4428c818e3dc7d05bbf
supasai(boy) <boyskypart@gmail.com>
2023-08-15 03:05:29 +0700

    add: brew casks

--- remove-chezmoi.sh
+++ remove-chezmoi.sh
@@ -3,5 +3,5 @@
 rm -rf ~/.config
 rm -rf ~/.local
 
-rm -rf ~/.asdf ~/.asdfrc ~/.gem ~/.gitconfig ~/.p10k.zsh ~/.p10k.zsh.zwc ~/.tmux
+rm -rf ~/.asdf ~/.asdfrc ~/.gem ~/.p10k.zsh ~/.p10k.zsh.zwc ~/.tmux
 rm -rf ~/.cache ~/.zlogin ~/.zsh_history ~/.zsh_sessions ~/.zshenv ~/.zshrc
