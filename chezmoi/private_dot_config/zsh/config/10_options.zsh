53c84231a1ec0f49cf183c276be04f7a4a63906a
supasai(boy) <boyskypart@gmail.com>
2023-08-15 13:54:25 +0700

    decorate zsh

--- chezmoi/private_dot_config/zsh/config/10_options.zsh
+++ chezmoi/private_dot_config/zsh/config/10_options.zsh
@@ -7,3 +7,16 @@ setopt extendedglob
 
 # Allow [ or ] whereever you want
 unsetopt nomatch
+
+# No duplicate history when reverse-searching my commands
+HISTSIZE=5000
+HISTFILE=~/.zsh_history
+SAVEHIST=5000
+HISTDUP=erase
+setopt appendhistory
+setopt sharehistory
+setopt incappendhistory
+setopt hist_ignore_all_dups
+setopt hist_save_no_dups
+setopt hist_ignore_dups
+setopt hist_find_no_dups
