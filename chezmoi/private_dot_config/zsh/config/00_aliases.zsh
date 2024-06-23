413bfe2bd88f7dfd4bc1378e84a8179c02df3a5d
supasai(boy) <boyskypart@gmail.com>
2024-06-23 14:58:20 +0700

    chore: set ls alias

--- chezmoi/private_dot_config/zsh/config/00_aliases.zsh
+++ chezmoi/private_dot_config/zsh/config/00_aliases.zsh
@@ -4,45 +4,47 @@ alias funcs='$EDITOR ~/.config/zsh/config/20_functions.zsh ; reload ; echo "Alia
 alias zshrc='$EDITOR ~/.zshrc ; reload'
 
 # Replace default commands
-alias ll='exa -la --git --icons'
-alias ls='exa'
-alias l='exa -lah'
 alias cat='bat'
-alias du='dust'
 alias df='dust -df'
+alias du='dust'
 alias find='fd'
 alias grep='rg'
+alias l='exa -lahF --git'
+alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'
+alias ll='exa -labF --git'
+alias ls='exa'
+alias lt='exa -lahbF --git --sort=modified --octal-permissions --time-style=long-iso'
+alias ping='prettyping --nolegend'
 alias top='htop'
 alias tree='exa --tree'
-alias ping='prettyping --nolegend'
-alias vim='nvim'
 alias vi='nvim'
+alias vim='nvim'
 
 # Git stuff
-alias gp="git push"
-alias gl="git pull"
-alias glog="git graph"
-alias gd='git diff'
 alias gaa="git add --all"
-alias gcd="git checkout develop"
-alias gcm="git checkout main"
-alias gst="git status -s -b && git log --oneline -n5 2>/dev/null || :"
-alias gs="git status -sb"
 alias gb="git branch"
 alias gba="git branch -a"
+alias gcd="git checkout develop"
+alias gcm="git checkout main"
 alias gcmsg="git commit -m"
 alias gco="git checkout"
+alias gd='git diff'
+alias gl="git pull"
+alias glog="git graph"
+alias gp="git push"
+alias gs="git status -sb"
+alias gst="git status -s -b && git log --oneline -n5 2>/dev/null || :"
 
 # directory short cuts
-alias p='cd ~/Projects/'
 alias d='cd ~/Downloads/'
-alias work="cd ~/work"
 alias dev="cd ~/dev"
+alias p='cd ~/Projects/'
+alias work="cd ~/work"
 
 # Misc
 alias .="cd .."
+alias active-sims="xcrun simctl list 'devices' 'booted'"
 alias cl="clear"
 alias ip="curl -s ipinfo.io/ip"
 alias reload="exec $SHELL -l"
-alias active-sims="xcrun simctl list 'devices' 'booted'"
 alias usage="du -h -d 1 | sort -h"
